bef7286ab05c6be65737dfb087f78d92313ce85b
datmino09 <datmino93737@gmail.com>
2025-03-25 12:06:47 +0700

    sửa searchFlight

--- src/controller/FlightController.js
+++ src/controller/FlightController.js
@@ -4,24 +4,24 @@ const axios = require('axios');
 
 class FlightController {
     // Tìm kiếm chuyến bay
-    async searchFlight(req, res) {
+    async searchFlight(req, res) { 
         try {
-            const { origin, destination, date } = req.query;
-            console.log("🔍 Tìm kiếm chuyến bay:", { origin, destination, date });
+            const { origin, destination, date, adults = 1, children = 0 } = req.query;
+            console.log("🔍 Tìm kiếm chuyến bay:", { origin, destination, date, adults, children });
+    
             if (!origin || !destination || !date) {
                 return res.status(400).json({ error: "Thiếu thông tin chuyến bay!" });
             }
-
+    
             const response = await requestWithRetry(`${AMADEUS_API}/v2/shopping/flight-offers`, 
-                { originLocationCode: origin, destinationLocationCode: destination, departureDate: date, adults: 1 });
-
+                { originLocationCode: origin, destinationLocationCode: destination, departureDate: date, adults, children });
+    
             res.json(response.data);
         } catch (error) {
             console.error("❌ Lỗi khi tìm chuyến bay:", error.response?.data || error.message);
             res.status(500).json({ error: error.response?.data || error.message });
         }
     }
-
     // Đặt vé
     async bookFlight(req, res) {
         const orderData = req.body;  // Nhận dữ liệu từ phía client
